72924ce13db41a826c6320c4056bec21f2b60a33
flubbypuppy <will.x.gao@gmail.com>
2022-10-13 14:30:56 -0400

    Removed color.color syntax

--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -10,22 +10,22 @@ type habitProps = {
 
 export default function Habit({name, initColor} : habitProps) {
   const [pickColor, setPickColor] = useState('')
-  const [color, setColor] = useState({color: initColor})
+  const [color, setColor] = useState(initColor)
   const [seeColor, setSeeColor] = useState(false)
 
   return (
     <span>
-      <p style={color}>
+      <p style={{color: color}}>
         {name}
       </p>
-      <input type="button" style={{backgroundColor: color.color}} onClick={() => setSeeColor(!seeColor)}/>
+      <input type="button" style={{backgroundColor: color}} onClick={() => setSeeColor(!seeColor)}/>
       <span>
         
       </span>
       {
         seeColor && <TwitterPicker
-          color={color.color}
-          onChangeComplete={ (color) => (setColor({color: color.hex}))} 
+          color={color}
+          onChangeComplete={ (result) => (setColor(result.hex))} 
         />
       }
     </span>

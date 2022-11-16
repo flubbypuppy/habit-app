ca0f8db8325daab6d1212747264b340f2afece92
flubbypuppy <will.x.gao@gmail.com>
2022-11-16 17:34:24 -0500

    Messed around with trying to make button have a border

--- src/components/AddHabit.tsx
+++ src/components/AddHabit.tsx
@@ -10,9 +10,14 @@ type addHabitProps = {
 
 export const AddHabit = ({handleChange, handleSubmit, text, disabled}: addHabitProps) => {
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="flex">       
+      <form className="flex py-1" onSubmit={handleSubmit}>
         <input className="bg-slate-400 rounded ml-2" type="text" id="New Habit" onChange={handleChange} value={text} maxLength={16}/>
-        <input className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border-slate-50 border-0" type="button" id="Submit Habit" value="Add Habit" disabled={disabled} onClick={handleSubmit}/>
-    </form>
+        <div className="rounded-full border-slate-50 border-1">
+          <input className="px-4 py-1 text-sm text-purple-600 font-semibold" type="button" id="Submit Habit" value="Add Habit" disabled={disabled} onClick={handleSubmit}/>
+        </div>
+      </form>
+    </div>
+
   )
 }
